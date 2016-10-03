e2003f7994ddff97af63e5fc13277f9f9bd810bf
Miles Florence <milesflo15@gmail.com>
2016-10-02 17:02:17 -0700

    Initial commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+import random
+strings = (["Hello Scott", "Hello Pax", "Hello Steve", "Hello World", "test"])
+n = random.randrange(1,len(strings))
+
+print strings[n]
---
+++ 13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print argv
+
+for i in argv:
+	print i
+
+print "The script is called:", script
+print "The first variable is:", first
+print "The second variable is:", second
+print "The third variable is:", third
